158bcfae04414a047c55e5c36e7ba6402eb7987c
Svensson22 <xeno.zohar@gmail.com>
2022-04-04 19:54:23 +0200

    Remove deprecated models

--- SeidoDbWebApiConsumer/Program.cs
+++ SeidoDbWebApiConsumer/Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
-using SeidoDbWebApiConsumer.Models;
 using SeidoDbWebApiConsumer.Services;
-
 using NecklaceModels;
 
 namespace SeidoDbWebApiConsumer
--- SeidoDbWebApiConsumer/Services/ISeidoDbHttpService.cs
+++ SeidoDbWebApiConsumer/Services/ISeidoDbHttpService.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using SeidoDbWebApiConsumer.Models;
-
 using NecklaceModels;
 
 namespace SeidoDbWebApiConsumer.Services
